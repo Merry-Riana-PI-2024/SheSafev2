40d0e435701c3840ce4220f9ed4f0a3b2631ed16
res Arfita <hresaarfita@gmail.com>
2024-10-27 11:17:05 +0700

    Tambahin komponen login-register, tambahin module css login-register, dan bikin Route-Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 
 import './App.css'
 import LandingPage from './pages/LandingPage'
+import Login from './components/Login-Regist/Login'
+import Register from './components/Login-Regist/Register'
+import AddJurnal from './pages/AddJurnal'
+import { Routes, Route } from 'react-router-dom'
+
 
 function App() {
 
   return (
     <>
-      <LandingPage />
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/regist" element={<Register />} />
+      <Route path="/addJurnal" element={<AddJurnal />} />
+    </Routes>
+
+      
     </>
   )
 }
---
+++ src/components/Login-Regist/Login.jsx
@@ -0,0 +1,63 @@
+import { Link } from "react-router-dom"
+import logo from "../../assets/images/lg_ss.png"
+import image from '../../assets/images/asset_login.png'
+import style from "../../assets/css/LoginRegist.module.css"
+import Navigation from "../Navigation"
+
+function Login() {
+    return(
+        <>
+        <div className="wrapper-mobile">
+            <div className="container mx-auto flex justify-center items-center min-h-screen p-4">
+                <div className="w-full max-w-lg">
+                    <Navigation />
+                    <div className="flex justify-center mb-6 mt-36">
+                        <img src={logo} alt="Logo" className="w-32" />
+                    </div>
+                    <div className="text-center mb-6">
+                      <h1 className={`${style['h1']} text-3xl font-bold`}>Login</h1>
+                      <p className={`${style['p']} mt-3 mb-3`}>Ruang Aman untuk Pemberdayaan dan Keadilan Perempuan</p>
+                    </div>
+                    <form action="">
+                        <div className="mb-4">
+                            <label htmlFor="email" className="block mb-2">Email</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="Email"
+                                required
+                                className={`${style['form-control']} w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-#8c263b-500`} />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="password" className="block mb-2">Password</label>
+                            <input 
+                                type="password"
+                                name="password"
+                                id="password"
+                                placeholder="Password"
+                                required
+                                className={`${style['form-control']} w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-#8c263b-500`} />
+                        </div>
+                        <div className="text-center">
+                            <button
+                              type="submit"
+                              className="px-6 py-2 sm-btn-primary mt-5">
+                              Login
+                            </button>
+                        </div>
+                    </form>
+                    <div className="mt-2 text-center mb-10">
+                      <small className={`${style['small']}`}>Belum Punya Akun? <Link to="/regist"><span className={`${style['small-b']}`}>Daftar</span></Link></small>
+                    </div>
+                </div>
+            </div>
+            <div className={`${style['hero-img']} mt-5`}>
+              <img className={`${style['img-1']} img-fluid`} src={image} />
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Login
